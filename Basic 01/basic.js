4e60d008e120086514cd1741d2a97210c117211e
Zenith_Avro <abuhasan323382@gmail.com>
2025-08-25 10:43:07 +0600

    basic discussion

---
+++ Basic 01/basic.js
@@ -0,0 +1,15 @@
+// => Defination of Machine learning.
+/**  Machine learning is the field of study that gives computer the ability to learn without being explicitly programmed.*/ 
+
+// => what is supervised learning? 
+//* Supervised learning is a type of machine learning where a model is trainded using a labeled dataset. 
+
+ //=> Some key point of the supervised learning - 
+    /* 
+        Data is labeled
+        Goal 
+        Training
+        Type of problem ( Classification & Regression ) */
+
+// => what is unsupervised learning?
+//* 
